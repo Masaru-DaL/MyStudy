d9a5aca9abd88257ef10f18f560ce1bd8a337355
Masaru <hittec9609@icloud.com>
2022-08-17 23:46:47 +0900

    paiza b

--- Paiza/paiza_b_2.py
+++ Paiza/paiza_b_2.py
@@ -1,1 +1,16 @@
-        
+# coding: utf-8
+# 自分の得意な言語で
+# Let's チャレンジ！！
+num_people, num_count = input().split()
+num_people = int(num_people)
+num_count = int(num_count)
+
+point_list = [''] * num_people
+
+
+for i in range(num_count):
+    point_list[i] = list(map(int, input().split()))
+
+print(point_list[0])
+print(point_list[1])
+print(point_list[2])
