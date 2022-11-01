c1aa2a8008171a28eb4ccd0de6768f8a1b4aa288
Masaru <hittec9609@icloud.com>
2022-11-01 14:34:10 +0900

    Python Class

---
+++ MyLearning/Memo/PythonClass/variable.py
@@ -0,0 +1,10 @@
+class TestClass:
+    morning = "Good Morning!!" # クラス変数
+    def greeting(self):
+        print(self.greeting)
+
+instance1 = TestClass()
+instance1.greeting()
+
+instance2 = TestClass()
+instance2.greeting()
