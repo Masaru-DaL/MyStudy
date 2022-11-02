73cb0046fec7ee6913d5896fc9113991429a8895
Masaru <hittec9609@icloud.com>
2022-11-03 06:50:20 +0900

    Serverless

--- MyLearning/Memo/PythonClass/class_merit.py
+++ MyLearning/Memo/PythonClass/class_merit.py
@@ -12,25 +12,28 @@
 # print(female1, "の平均点は", female1_average, "です。")
 
 # 平均点を出す関数
-def average(name, japanese, english):
-    average_point = (japanese + english) / 2
-    print(name, "の平均点は", average_point, "です。")
+# def average(name, japanese, english):
+#     average_point = (japanese + english) / 2
+#     print(name, "の平均点は", average_point, "です。")
 
-man1 = "Bob"
-female1 = "Lisa"
+# man1 = "Bob"
+# female1 = "Lisa"
 
-average(man1, 60, 90)
-average(female1, 100, 70)
+# average(man1, 60, 90)
+# average(female1, 100, 70)
 
-# # クラスを定義する
-# class Average():
-#     # クラス内に関数を定義します
-#     def average(self, name, japanese, english):
-#         average_point = (japanese + english) / 2
-#         print(name, "の平均点は", average_point, "です。")
+# クラスを定義する
+class Average():
+    # クラス内に関数を定義します
+    def average(self, man1, female1, japanese, english):
+        average_point = (japanese + english) / 2
+        self.man1 = "Bob"
+        self. female1 = "Lisa"
+        print(man1, "の平均点は", average_point, "です。")
+        print(female1, "の平均点は", average_point, "です。")
 
-# man1 = Average() # 変数man1にAverageクラスを実装する
-# man1.average("Bob", 60, 90)
+man1 = Average() # 変数man1にAverageクラスを実装する
+man1.average(60, 90)
 
-# female1 = Average()
-# female1.average("Lisa", 100, 70)
+female1 = Average()
+female1.average(100, 70)
