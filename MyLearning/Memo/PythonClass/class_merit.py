3322507fdaaf381967cfb295f41628233fd64a22
Masaru <hittec9609@icloud.com>
2022-11-02 17:33:50 +0900

    Serverless

---
+++ MyLearning/Memo/PythonClass/class_merit.py
@@ -0,0 +1,12 @@
+man1 = "Bob"
+man1_japanese = 60
+man1_english = 90
+man1_average = (man1_japanese + man1_english) / 2
+
+female1 = "Lisa"
+female1_japanese = 100
+female1_english = 70
+female1_average = (female1_japanese + female1_english) / 2
+
+print(man1, "の平均点は", man1_average, "です。")
+print(female1, "の平均点は", female1_average, "です。")
