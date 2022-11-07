14495eb37dabddbc8845daa0d0503d3eb241d952
Masaru <hittec9609@icloud.com>
2022-11-08 06:00:26 +0900

    Git

--- MyLearning/Memo/PythonClass/Human.py
+++ MyLearning/Memo/PythonClass/Human.py
@@ -7,4 +7,6 @@ human.name = "Mike"
 human.age = 20
 human.height = 180
 
-print("名前:", human.name, "年齢:", human.age, "身長:", dog.height, "cm")
+print("名前:", human.name, "年齢:", human.age, "身長:", human.height, "cm")
+
+ate t tratar r
