e47a0358aec9bcaba7c13b776c67c109fab69e51
Masaru <hittec9609@icloud.com>
2022-11-11 13:32:43 +0900

    del done enter the commit message for your changes. Lines starting

--- MyLearning/TechnicalArticles-Public/Python-list/del.py
+++ MyLearning/TechnicalArticles-Public/Python-list/del.py
@@ -1,7 +1,16 @@
-sample_list = ["a", "b", "c", "d", "e"]
+sample_list = ["A", "B", "C", "D", "E"]
 
-del sample_list[0:2]
+# ①index0を指定
+del sample_list[0]
 print(sample_list)
+# 出力結果: ['B', 'C', 'D', 'E']
 
+# ②index1~-1の1個前まで
+del sample_list[1:-1]
+print(sample_list)
+# 出力結果: ['B', 'E']
+
+# ③リスト自体を削除
 del sample_list
 print(sample_list)
+# 出力結果: 
