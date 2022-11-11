e6e7e601eddad54e832cf80543f328987d53471e
Masaru <hittec9609@icloud.com>
2022-11-11 11:49:49 +0900

    basic2 done

---
+++ MyLearning/TechnicalArticles-Public/Python-list/basic2.py
@@ -0,0 +1,17 @@
+sample_list = ["a", "b", "c", "d", "e"]
+
+# index0~4の1個前まで
+print(sample_list[0:4])
+# 出力結果: ['a', 'b', 'c', 'd']
+
+# index-1~全て
+print(sample_list[-1:])
+# 出力結果: ['e']
+
+# index2~6の1個前まで
+print(sample_list[2:6])
+# 出力結果: ['c', 'd', 'e']
+
+# index5~全て
+print(sample_list[5:])
+# 出力結果: []
