291975bf17006a0a3e391acf1fda15f75708276b
Masaru <hittec9609@icloud.com>
2022-11-11 13:38:12 +0900

    in done

--- MyLearning/TechnicalArticles-Public/Python-list/in.py
+++ MyLearning/TechnicalArticles-Public/Python-list/in.py
@@ -1,4 +1,9 @@
-sample_list = ["a", "b", "c", "d", "e"]
+sample_list = ["A", "B", "C", "D", "E"]
 
-print("d" in sample_list)
-print("y" in sample_list)
+# ①要素Aを検索
+print("A" in sample_list)
+# 出力結果: True
+
+# ②存在しない要素Xを検索
+print("X" in sample_list)
+# 出力結果: False
