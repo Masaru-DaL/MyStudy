df5d53aafd34e0d5196d72276e112dc02c7a28e2
Masaru <hittec9609@icloud.com>
2022-11-11 13:45:55 +0900

    index done

--- MyLearning/TechnicalArticles-Public/Python-list/index.py
+++ MyLearning/TechnicalArticles-Public/Python-list/index.py
@@ -1,3 +1,9 @@
-sample_list = ["a", "b", "c", "d", "e"]
+sample_list = ["A", "B", "C", "D", "E"]
 
-print(sample_list.index("e"))
+# ①要素Eを指定
+print(sample_list.index("E"))
+# 出力結果: 4
+
+# ②存在しないXを指定
+print(sample_list.index("X"))
+# 出力結果: ValueError: 'X' is not in list
