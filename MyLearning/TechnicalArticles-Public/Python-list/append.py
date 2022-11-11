70914454aec6fbf06cbccd4f33984fce75850045
Masaru <hittec9609@icloud.com>
2022-11-11 12:04:57 +0900

    append add code

--- MyLearning/TechnicalArticles-Public/Python-list/append.py
+++ MyLearning/TechnicalArticles-Public/Python-list/append.py
@@ -1,11 +1,16 @@
-sample_list = ["a", "b", "c", "d", "e"]
+sample_list = ["a", "b", "c"]
 
 # ①
-sample_list.append("f")
+sample_list.append("d")
 print(sample_list)
-# 出力結果: ['a', 'b', 'c', 'd', 'e', 'f']
+# 出力結果: ['a', 'b', 'c', 'd']
 
 # ②
+sample_list.append(["e", "f"])
+print(sample_list)
+# 出力結果: ['a', 'b', 'c', 'd', ['e', 'f']]
+
+# ③
 sample_list.append("100")
 print(sample_list)
-# 出力結果: ['a', 'b', 'c', 'd', 'e', 'f', '100']
+# 出力結果: ['a', 'b', 'c', 'd', ['e', 'f'], '100']
