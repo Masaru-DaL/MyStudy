39c17b09b00af4e6e7694cff8d2b22a9b94a16c9
Masaru <hittec9609@icloud.com>
2022-11-11 11:24:02 +0900

    index specify done

---
+++ MyLearning/TechnicalArticles-Public/Python-list/basic1.py
@@ -0,0 +1,11 @@
+sample_list = ["a", "b", "c", "d", "e"]
+
+print(sample_list[0])
+# 出力結果: a
+
+print(sample_list[4])
+print(sample_list[-1])
+# 出力結果: e
+
+print(sample_list[6])
+# 出力結果:　IndexError: list index out of range
