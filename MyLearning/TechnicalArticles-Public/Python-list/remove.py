62ade8122e615fb40b374e2a96f0c4b1dc186ba4
Masaru <hittec9609@icloud.com>
2022-11-11 13:20:02 +0900

    add code

--- MyLearning/TechnicalArticles-Public/Python-list/remove.py
+++ MyLearning/TechnicalArticles-Public/Python-list/remove.py
@@ -11,6 +11,6 @@ print(sample_list)
 # 出力結果: ['B', 'D', 'E']
 
 # ③存在しない要素を指定する
-sample_list.remove("A")
+sample_list.remove("Z")
 print(sample_list)
 # 出力結果: ValueError: list.remove(x): x not in list
