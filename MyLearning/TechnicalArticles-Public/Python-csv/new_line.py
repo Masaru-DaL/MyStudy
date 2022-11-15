81af86e34f054ff978fe14d73a00ff199a239f9d
Masaru <hittec9609@icloud.com>
2022-11-15 12:03:15 +0900

    newline not done

---
+++ MyLearning/TechnicalArticles-Public/Python-csv/new_line.py
@@ -0,0 +1,7 @@
+import csv
+
+with open("sample.csv", encoding="UTF-8") as file_object:
+    reader_object = csv.reader(file_object, delimiter=" ")
+
+    for row in reader_object:
+        print(row)
