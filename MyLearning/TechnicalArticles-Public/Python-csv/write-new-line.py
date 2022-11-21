e5ceef503fb69197e572936b1d939e7e5eae7e5c
Masaru <hittec9609@gmail.com>
2022-11-21 12:33:42 +0900

    Add newline

---
+++ MyLearning/TechnicalArticles-Public/Python-csv/write-new-line.py
@@ -0,0 +1,20 @@
+import csv
+
+# ①書き込む二次元リストを用意（改行コードを含む）
+write_list = [[1, 2, 3], ["A\n", "B", "C"], [4, "\n5", "6"]]
+
+# ②ここから書き込み
+with open("sample.csv", "w", newline='') as file_object:
+    writer_object = csv.writer(file_object)
+    writer_object.writerows(write_list)
+
+# ③ここから読み込み（書き込まれたことの確認）
+with open("sample.csv") as file_object:
+    print(file_object.read())
+
+# ④出力結果:
+# 1,2,3
+# "A
+# ",B,C
+# 4,"
+# 5",6
